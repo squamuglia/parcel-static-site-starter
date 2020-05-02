03bf473fc7c999c310e58ab0186e8beee201c9f8
squamuglia <maxsmouha@gmail.com>
2020-05-02 11:23:40 -0400

    add purgecss and rimraf

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,25 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+const autoprefixer = require('autoprefixer');
+const postcsspresetenv = require('postcss-preset-env');
+
 module.exports = {
-	plugins: {
-		autoprefixer: {},
-		'postcss-preset-env': {
+	plugins: [
+		purgecss({
+			content: ['src/**.html'],
+			css: ['src/styles/**.css'],
+		}),
+		autoprefixer,
+		postcsspresetenv({
 			stage: 0,
 			features: {
 				'nesting-rules': true,
 			},
-		},
-		'postcss-import': {},
-		'postcss-nested': {},
-		'postcss-calc': {},
-		'postcss-discard-comments': {},
-		'postcss-reporter': {},
-		'postcss-custom-media': {},
-	},
+		}),
+		'postcss-import',
+		'postcss-nested',
+		'postcss-calc',
+		'postcss-discard-comments',
+		'postcss-reporter',
+		'postcss-custom-media',
+	],
 };
